b9fc986b7a1f1931811a63709867f0dd1f6ba48a
SonLam <lamthaison.dev@gmail.com>
2021-02-25 09:53:20 +0700

    learning ngClass-property-binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,9 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styles: [`
+      .online {
+        color: white;
+        background-color: goldenrod;
+        padding: 8px 12px;
+      }
+    `]
 })
 export class ServerComponent {
   serverID = 10;
-  serverStatus = true;
+  serverStatus = 'offline';
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
 }
 
