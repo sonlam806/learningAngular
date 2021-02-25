ecca297973761e313dc9b5003831707463857ba0
SonLam <lamthaison.dev@gmail.com>
2021-02-25 09:01:42 +0700

    finish excercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'shindo';
+  canClick = false;
+  username = '';
+
+  onReset() {
+    this.username = '';
+  }
 }
