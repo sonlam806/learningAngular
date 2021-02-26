04d5153f1f5ec6b6b515187bdf08312e2bd7c2be
SonLam <lamthaison.dev@gmail.com>
2021-02-26 14:53:41 +0700

    fix server-element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements: any = [];
+  serverElements = [{type: 'server', name: 'development server', content: 'Local server for FE dev team'}];
   newServerName = '';
   newServerContent = '';
 
-  onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
-
-  onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
 }
