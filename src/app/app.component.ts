fb093775f25e1265e34074771cc36104fc79bd9e
SonLam <lamthaison.dev@gmail.com>
2021-02-26 14:58:10 +0700

    add more dummy data to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [{type: 'server', name: 'development server', content: 'Local server for FE dev team'}];
+  serverElements = [
+    {type: 'server', name: 'development server', content: 'Local server for FE dev team'},
+  {type: 'blueprint', name: 'production staging server', content: 'Server for testing in production'}
+  ];
   newServerName = '';
   newServerContent = '';
 
