7b7313da01b1f6e203c2febe60d3ef80911eae94
SonLam <lamthaison.dev@gmail.com>
2021-02-26 19:46:51 +0700

    learning eventEmitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   serverElements = [
-    {type: 'server', name: 'development server', content: 'Local server for FE dev team'},
-  {type: 'blueprint', name: 'production staging server', content: 'Server for testing in production'}
+    {
+      type: 'server',
+      name: 'development server',
+      content: 'Local server for FE dev team',
+    },
+    {
+      type: 'blueprint',
+      name: 'production staging server',
+      content: 'Server for testing in production',
+    },
   ];
   newServerName = '';
   newServerContent = '';
 
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+
+  onBlueprintAdded(blueprintData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent,
+    });
+  }
 }
