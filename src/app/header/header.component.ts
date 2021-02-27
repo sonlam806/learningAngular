608aec002ec7a447027c0c34b461d5a99cf76301
SonLam <lamthaison.dev@gmail.com>
2021-02-27 14:50:10 +0700

    add navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,16 @@
-import {Component} from "@angular/core";
+import {Component, EventEmitter, Output} from "@angular/core";
 
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  styleUrls : ['./header.component.css']
 })
 export class HeaderComponent {
+  @Output() locationChanged = new EventEmitter<string>();
 
+  navigateTo(location: string) {
+    this.locationChanged.emit(location)
 
+  }
 }
