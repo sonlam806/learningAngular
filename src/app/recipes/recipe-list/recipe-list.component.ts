3349b74d979f90354f237ae4d7167a6b0c0d9a38
SonLam <lamthaison.dev@gmail.com>
2021-03-01 08:33:36 +0700

    binding data for recipes component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from '../recipe.model'
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Hambuger', 'This is simply a test','http://assets.epicurious.com/photos/57c5c6d9cf9e9ad43de2d96e/master/pass/the-ultimate-hamburger.jpg' )
+    new Recipe('Hambuger', 'This is simply a test','http://assets.epicurious.com/photos/57c5c6d9cf9e9ad43de2d96e/master/pass/the-ultimate-hamburger.jpg' ),
+    new Recipe('Hamburger 2', 'This is simply a copy','http://assets.epicurious.com/photos/57c5c6d9cf9e9ad43de2d96e/master/pass/the-ultimate-hamburger.jpg' )
   ];
 
   constructor() { }
