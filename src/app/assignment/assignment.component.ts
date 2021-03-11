09a7aff4d714e51a89e00a9c8429e8fc6d765044
SonLam <thaisonlamsp@gmail.com>
2021-03-11 09:00:20 +0700

    add async validate function to check email field

--- src/app/assignment/assignment.component.ts
+++ src/app/assignment/assignment.component.ts
@@ -1,34 +1,64 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {
+  AsyncValidatorFn,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-assignment',
   templateUrl: './assignment.component.html',
-  styleUrls: ['./assignment.component.css']
+  styleUrls: ['./assignment.component.css'],
 })
 export class AssignmentComponent implements OnInit {
   // @ts-ignore
   projectForm: FormGroup;
-  forbiddenProjectName = ['Test'];
+  forbiddenProjectName = ['test'];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.projectForm = new FormGroup({
-      projectName: new FormControl(null, [Validators.required, this.validateProjectName.bind(this)]),
-      email: new FormControl(null, [Validators.required, Validators.email]),
-      projectStatus: new FormControl('stable')
-    })
+      projectName: new FormControl(null, [
+        Validators.required,
+        this.validateProjectName.bind(this),
+      ]),
+      email: new FormControl(
+        null,
+        [Validators.required, Validators.email],
+        // @ts-ignore
+        this.validateEmail
+      ),
+      projectStatus: new FormControl('stable'),
+    });
   }
 
   onSubmit() {
-    console.log(this.projectForm.get('projectName'))
+    console.log(this.projectForm);
   }
 
   validateProjectName(control: FormControl): { [key: string]: boolean } | null {
-    if(this.forbiddenProjectName.includes(control.value)) {
-    return {invalid: true}
-    }
-    else return null;
+    if (this.forbiddenProjectName.includes(control.value?.toLowerCase())) {
+      return { invalid: true };
+    } else return null;
+  }
+
+  validateEmail(
+    control: FormControl
+  ): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+    const validatePromise = new Promise<ValidationErrors | null>((resolve) => {
+      setTimeout(() => {
+        if (control.value?.toLowerCase() === 'shindo@shindo.com') {
+          resolve({ invalid: true });
+        } else {
+          resolve(null);
+        }
+      }, 2000);
+    });
+    return validatePromise;
   }
 }
+
