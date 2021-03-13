276fc47da26756591fd32dc92a58d5dd868c4df8
SonLam <thaisonlamsp@gmail.com>
2021-03-13 14:12:46 +0700

    add custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServerService } from './servers/server.service';
+import { TruncatePipe } from './truncate.pipe';
 
 @NgModule({
-  declarations: [AppComponent, ServersComponent, ServerComponent],
+  declarations: [AppComponent, ServersComponent, ServerComponent, TruncatePipe],
   imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [ServerService],
   bootstrap: [AppComponent],
