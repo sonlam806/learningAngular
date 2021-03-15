5d0e01cd114bb1d54b2f4775279668bd0ae90a8c
SonLam <thaisonlamsp@gmail.com>
2021-03-15 11:33:08 +0700

    add initial layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  onCreatePost(post: Post) {
+    this.http
+      .post(
+        'https://angular-http-request-abcd3-default-rtdb.firebaseio.com/posts.json',
+        post
+      )
+      .subscribe((responseData) => console.log(responseData));
+  }
+
+  onFetchPosts() {}
+
+  onClearPosts() {}
+}
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,7 +7,7 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
